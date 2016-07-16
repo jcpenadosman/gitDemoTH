0daa98caa0656c5bcee48b33bad0a8bf6c9cf388
jcpenadosman <jcpenadosman@gmail.com>
2016-07-15 21:18:39 -0500

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Juan Camilo Peña D. on 7/15/16.
+//  Copyright © 2016 Juan Camilo Peña D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
